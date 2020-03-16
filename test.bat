11385c98e9f3c5e5cbbce7e37167bb010d5b287c
JohnnyCatsvill <56757938+JohnnyCatsvill@users.noreply.github.com>
2020-03-16 08:19:10 +0300

    Add files via upload

--- test.bat
+++ test.bat
@@ -1,12 +1,12 @@
 Debug\GameOfLife.exe gen0.txt gen1.txt
-echo ERROR=%ERRORLEVEL%
+echo ERROR=%ERRORLEVEL% ? 0
 
 Debug\GameOfLife.exe gen1.txt
-echo ERROR=%ERRORLEVEL%
+echo ERROR=%ERRORLEVEL% ? 0
 
 Debug\GameOfLife.exe
-echo ERROR=%ERRORLEVEL%
+echo ERROR=%ERRORLEVEL% ? 1
 
 Debug\GameOfLife.exe notexists.txt
-echo ERROR=%ERRORLEVEL%
+echo ERROR=%ERRORLEVEL% ? 2
 pause
